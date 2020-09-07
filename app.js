d7ab1d039e3569bbf20e089a87472bbefcbb8f04
kawasaki <k.kawasaki0820@gmail.com>
2020-09-07 17:04:31 +0900

    メモ化によるアルゴルズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 'use strict';
 
+var fibMap = new Map();
+
 function fib(n) {
   if (n == 0) {
+    fibMap.set(0,0);
     return 0;
   } else if (n == 1) {
+    fibMap.set(1,1);
     return 1;
   } else {
-    return fib(n-1) + fib(n-2);
+    let sum = fibMap.get(n-1) + fibMap.get(n-2);
+    fibMap.set(n, sum);
+    return sum;
   }
 }
 
@@ -14,3 +20,18 @@ const length = 40;
 for(let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
+
+// function fib(n) {
+//   if (n == 0) {
+//     return 0;
+//   } else if (n == 1) {
+//     return 1;
+//   } else {
+//     return fib(n-1) + fib(n-2);
+//   }
+// }
+
+// const length = 40;
+// for(let i = 0; i <= length; i++) {
+//   console.log(fib(i));
+// }
